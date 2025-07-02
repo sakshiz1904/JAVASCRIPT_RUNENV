c0481d6c9aa64d1c37b35591f78055d4a47ffe2c
sakshiz1904 <162597878+sakshiz1904@users.noreply.github.com>
2025-07-02 12:37:28 +0000

    variable in JS

---
+++ basicsofJS/variable.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "sakshizade1904@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 2 Not allowed
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+accountstate = "Maharashtra"
+console.log(accountstate);
