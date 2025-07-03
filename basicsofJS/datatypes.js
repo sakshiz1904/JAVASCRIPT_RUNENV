f597bb65e4fbb710ea638052fa520497f7d190e7
sakshiz1904 <162597878+sakshiz1904@users.noreply.github.com>
2025-07-03 12:06:11 +0000

    datatype

---
+++ basicsofJS/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) //we are using node.js, not browser
+
+/*Primitive Datatypes
+    1. number => 2 to power 53
+    bigint 
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefinedd =>
+    symbol => unique 
+
+*/
+console.log(typeof null); //object 
+console.log(typeof undefined); //undefined
